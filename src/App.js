0f01b1ca6ab9fd00c5a86191d9949b6a8050359b
Dominique Reese <dreese@microsoft.com>
2021-09-20 14:56:29 -0400

    Initial inclusion of material-ui

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import Button from '@mui/material/Button'
+import Typography from '@mui/material/Typography'
+import Box  from '@mui/system/Box';
 
 function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+  const value = 'CPX';
+  return (<
+    Box sx={{ width: '100%', maxWidth: 500}} >
+      <Typography variant="h1" component="div" gutterBottom>
+        Hello {value}
+      </Typography>
+  </Box>);
 }
 
 export default App;
