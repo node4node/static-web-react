9f38e862d595078ebf94a7cf5228a0f522efa737
node4node <64428260+node4node@users.noreply.github.com>
2021-09-20 15:10:57 -0400

    Initial inclusion of material-ui (#1)
    * Initial inclusion of material-ui
    
    * Getting rid of unused code
    
    Co-authored-by: Dominique Reese <dreese@microsoft.com>

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import Typography from '@mui/material/Typography'
+import Box  from '@mui/system/Box';
 
 function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+  const value = 'CPX';
+  return (<
+    Box sx={{ width: '100%', maxWidth: 500}} >
+      <Typography variant="h1" component="div" gutterBottom>
+        Hello {value}
+      </Typography>
+  </Box>);
 }
 
 export default App;
